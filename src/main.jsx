5971fad574b8770ef16c821016781afede5036be
maheshchikkam <cmnaidu1206@gmail.com>
2025-08-22 22:44:49 +0530

    Updated styles.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import './Styles/variables.css';
+import './styles/variables.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
