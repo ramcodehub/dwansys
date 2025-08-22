c759971024ce94616b6311d1068687d31b5a7e75
maheshchikkam <cmnaidu1206@gmail.com>
2025-08-22 15:25:52 +0530

    Added bootstrap.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <div className="container mt-5">
+        <h1 className="text-primary">Hello, Bootstrap 5 + Vite + React 🚀</h1>
+        <button className="btn btn-success">Click Me</button>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
