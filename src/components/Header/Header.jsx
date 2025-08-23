e6f5ceeef062bc7563bfdb5c91f822429356292c
maheshchikkam <cmnaidu1206@gmail.com>
2025-08-23 13:23:29 +0530

    Formatted.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,58 +9,100 @@ const Header = () => {
       <img src={logo} alt="Logo" />
       <nav className="header-nav">
         <ul>
-        <li className='dropdown'>
-          <a href="#" className="header-link">
-            Data Solutions <i class="bi bi-chevron-down dropdown-icon"></i>
-          </a>
-          <ul>
-            <li><a href="#">Data Ingestion</a></li>
-            <li><a href="#">Data Storage</a></li>
-            <li><a href="#">Data Transformation</a></li>
-            <li><a href="#">Data Migration Services</a></li>
-            <li><a href="#">Data Modelling</a></li>
-            <li><a href="#">Data Orchestration</a></li>
-          </ul>
-        </li>
-        <li className='dropdown'>
-          <a href="#" className="header-link">
-            Data Analytics <i class="bi bi-chevron-down dropdown-icon"></i>
-          </a>
-          <ul>
-            <li><a href="#">Data Ingestion</a></li>
-            <li><a href="#">Data Storage</a></li>
-            <li><a href="#">Data Transformation</a></li>
-            <li><a href="#">Data Migration Services</a></li>
-            <li><a href="#">Data Modelling</a></li>
-            <li><a href="#">Data Orchestration</a></li>
-          </ul>
-        </li>
-          
-        <li className='dropdown'>
-          <a href="#" className="header-link">
-            AI Solutions <i class="bi bi-chevron-down dropdown-icon"></i>
-          </a>
-          <ul>
-            <li><a href="#">Data Ingestion</a></li>
-            <li><a href="#">Data Storage</a></li>
-            <li><a href="#">Data Transformation</a></li>
-            <li><a href="#">Data Migration Services</a></li>
-            <li><a href="#">Data Modelling</a></li>
-            <li><a href="#">Data Orchestration</a></li>
-          </ul>
-        </li>
-        <li><a href="#" className="header-link">
-          Explore dwansys
-        </a></li>
-        <li><a href="#" className="header-link">
-          Blogs
-        </a></li>
-        <li><a href="#" className="header-link">
-          Careers
-        </a></li>
+          <li className="dropdown">
+            <a href="#" className="header-link">
+              Data Solutions <i class="bi bi-chevron-down dropdown-icon"></i>
+            </a>
+            <ul>
+              <li>
+                <a href="#">Data Ingestion</a>
+              </li>
+              <li>
+                <a href="#">Data Storage</a>
+              </li>
+              <li>
+                <a href="#">Data Transformation</a>
+              </li>
+              <li>
+                <a href="#">Data Migration Services</a>
+              </li>
+              <li>
+                <a href="#">Data Modelling</a>
+              </li>
+              <li>
+                <a href="#">Data Orchestration</a>
+              </li>
+            </ul>
+          </li>
+          <li className="dropdown">
+            <a href="#" className="header-link">
+              Data Analytics <i class="bi bi-chevron-down dropdown-icon"></i>
+            </a>
+            <ul>
+              <li>
+                <a href="#">Data Ingestion</a>
+              </li>
+              <li>
+                <a href="#">Data Storage</a>
+              </li>
+              <li>
+                <a href="#">Data Transformation</a>
+              </li>
+              <li>
+                <a href="#">Data Migration Services</a>
+              </li>
+              <li>
+                <a href="#">Data Modelling</a>
+              </li>
+              <li>
+                <a href="#">Data Orchestration</a>
+              </li>
+            </ul>
+          </li>
+
+          <li className="dropdown">
+            <a href="#" className="header-link">
+              AI Solutions <i class="bi bi-chevron-down dropdown-icon"></i>
+            </a>
+            <ul>
+              <li>
+                <a href="#">Data Ingestion</a>
+              </li>
+              <li>
+                <a href="#">Data Storage</a>
+              </li>
+              <li>
+                <a href="#">Data Transformation</a>
+              </li>
+              <li>
+                <a href="#">Data Migration Services</a>
+              </li>
+              <li>
+                <a href="#">Data Modelling</a>
+              </li>
+              <li>
+                <a href="#">Data Orchestration</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#" className="header-link">
+              Explore dwansys
+            </a>
+          </li>
+          <li>
+            <a href="#" className="header-link">
+              Blogs
+            </a>
+          </li>
+          <li>
+            <a href="#" className="header-link">
+              Careers
+            </a>
+          </li>
         </ul>
       </nav>
-      <Button variant='secondary' children='Get Started'/>
+      <Button variant="secondary" children="Get Started" />
     </header>
   );
 };
