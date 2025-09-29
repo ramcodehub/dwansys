9ad2752d865dd9ff85e588eb619101e0beb316e7
ramcodehub <ramcodehub@gmail.com>
2025-09-29 17:35:47 +0530

    All done

--- frontend/src/pages/UseCases/BankingAndFinancialServices/AIPoweredComplianceAndAntiMoneyLaundering.jsx
+++ frontend/src/pages/UseCases/BankingAndFinancialServices/AIPoweredComplianceAndAntiMoneyLaundering.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PageBuilder from '../../../components/PageBuilder/PageBuilder'
-import AIPoweredComplianceAndAntiMoneyLaunderingData from '../../../config/UseCases/BankingAndFinancialServices/AIPoweredComplianceAndAntiMoneyLaunderingData'
+import AIPoweredComplianceAndAntiMoneyLaunderingData from '../../../config/UseCases/BankingandFinancialServices/AIPoweredComplianceAndAntiMoneyLaunderingData'
 
 const AIPoweredComplianceAndAntiMoneyLaundering = () => {
   return (
